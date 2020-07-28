7ad1fa1e92461e69b2b9346bd2bd2c3dde380662
GiacomoDevIT <45907453+GiacomoDevIT@users.noreply.github.com>
2020-07-28 20:22:03 +0200

    Add files via upload

--- antiddos.php
+++ antiddos.php
@@ -1,18 +1,14 @@
 <?php
-if(!$_COOKIE['challenge']){
+function hash_password($password)
+{
+  define('key', 'random_key');
+  $hashed_password = hash("SHA256", $password . key);
+  return $hashed_password;
+}
+if(!$_COOKIE['challenge'] == hash_password('passed')){
 ?>
 <script async="" defer="" type='text/javascript'>
 window.onload=async function(){
-async function readCookie(name) {
-    var nameEQ = await name + "=";
-    var ca = await document.cookie.split(';');
-    for(var i= await 0;i < ca.length;i++) {
-        var c = await ca[i];
-        while (c.charAt(0)==' ') c = await c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return await c.substring(nameEQ.length,c.length);
-    }
-    return await null;
-}
 
 async function setCookie(name, value, maxAgeSeconds) {
 	var maxAgeSegment = await "; max-age=" + maxAgeSeconds;
@@ -25,16 +21,16 @@ await document.write("<meta name=\"viewport\" content=\"width=device-width, init
 await document.write("<title>LucaDev - Challenge<\/title>");
 await document.write("<\/head>");
 await document.write("<body>");
-await document.write("<br><\/br><br><h1 align=center>Checking your browser before accessing... <\/h1>");
-await document.write("<h3 align=center>This process is automatic. Your browser will redirect to your requested content shortly.<\/h3>");
+await document.write("<br><\/br><br><h1 align=center>Checking your browser before accessing...<\/h1>");
 await document.write("<h4 align=center>Please allow up to 3 seconds...<\/h4>");
 await document.write("<h6 align=center>DDoS protection by <a href=\"https:\/\/t.me\/joinchat\/AAAAAFhp0uVYKNeFEivyzw\">LucaDev<\/a><\/h6>");
 await document.write("<\/body>");
 setTimeout(async function(){ 
-	await setCookie('challenge', 'passed', 30);
+	await setCookie('challenge', "<?php echo hash_password('passed'); ?>", 30);
 	await location.reload();
 }, 800);
 };
+
 </script>
 <?php
 die();
