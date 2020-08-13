a7d5d7f375ddd7f5d6cde8c66fcc128888b2a384
Hayden Nicholson Goult <54115165+h-ng@users.noreply.github.com>
2020-08-13 08:54:02 +0100

    Changed antiddos.php to the hngCMS version.

--- antiddos.php
+++ antiddos.php
@@ -1,35 +1,33 @@
 <?php
+include 'config/config.php';
+$antiddosver = $_HC['antiddosver'];
+$sitename = $_HC['sitename'];
+$url = $_HC['domain'];
+$key = $_HC['key'];
 function hash_password($password)
 {
-  define('key', 'random_key');
-  $hashed_password = hash("SHA256", $password . key);
-  return $hashed_password;
+  define('key', "$key");
+  $hashed_password1 = hash("SHA256", $password . key);
+  return $hashed_password1;
 }
 if($_COOKIE['challenge'] != hash_password('passed')){
 ?>
 <script async="" defer="">
 window.onload=async function(){
 async function setCookie(name, value, maxAgeSeconds) {
-	var maxAgeSegment = await "; max-age=" + maxAgeSeconds;
-	document.cookie = await encodeURI(name) + "=" + await encodeURI(value) + await maxAgeSegment;
+        var maxAgeSegment = await "; max-age=" + maxAgeSeconds;
+        document.cookie = await encodeURI(name) + "=" + await encodeURI(value) + await maxAgeSegment;
 }
-await document.write("<head>");
-await document.write("<meta charset=\"UTF-8\">");
-await document.write("<meta name=\"author\" content=\"LucaDev Team\">");
-await document.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-await document.write("<title>LucaDev - Challenge<\/title>");
-await document.write("<\/head>");
-await document.write("<body>");
 await document.write("<br><\/br><br><h1 align=center>Checking your browser before accessing...<\/h1>");
 await document.write("<h4 align=center>Please allow up to 3 seconds...<\/h4>");
-await document.write("<h6 align=center>DDoS protection by <a href=\"https:\/\/t.me\/joinchat\/AAAAAFhp0uVYKNeFEivyzw\">LucaDev<\/a><\/h6>");
-await document.write("<\/body>");
+await document.write("<p align=center>DDoS ray protection by <?php echo $sitename; ?> for <?php echo $url; ?><\/p>");
 setTimeout(async function(){ 
-	await setCookie('challenge', "<?php echo hash_password('passed'); ?>", 30);
-	await location.reload();
-}, 800);
+        await setCookie('challenge', "<?php echo hash_password('passed'); ?>", 300);
+        await location.reload();
+}, 1000);
 };
 </script>
+<pre>JavaScript is required, Please enable, this is enforced by  <?php echo $sitename;?></pre>
 <?php
 die();
 }
